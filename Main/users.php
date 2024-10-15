bb28ae5a32e4a68a4d6f5cc4d9d746c3176f7439
Markk-dev <markvincent.dev@gmail.com>
2024-10-16 03:43:19 +0800

    Added adjustments and fix minor issues

--- Main/users.php
+++ Main/users.php
@@ -4,39 +4,68 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users</title>
-    <link rel="stylesheet" href="../Sub/user.css">
+    <link rel="stylesheet" href="../Sub/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+
+
 </head>
 <body>
-    <h2>Registered Users</h2>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Last Name</th>
-            <th>First Name</th>
-            <th>Email</th>
-            <th>Action</th>
-        </tr>
 
-        <?php
-        include 'db.php';
+<div class="Cover">
 
-        $result = $conn->query("SELECT * FROM users"); 
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>
-                    <td>{$row['id']}</td>
-                    <td>{$row['lname']}</td>
-                    <td>{$row['fname']}</td>
-                    <td>{$row['email']}</td>
-                    <td>
-                        <a href='edit.php?id={$row['id']}'>Edit</a>
-                        <a href='delete.php?id={$row['id']}'>Delete</a>
-                    </td>
-                </tr>";
-        }
-        ?>
+    <p>Registered Users</p>
 
-    </table>
-    <a href='../Main/register.php'>Register New User</a>
+    <div class="btn">
+        <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+        <a href='../Main/register.php' class="Regbtn">Register New</a>
+    </div>
+
+    <div class="Regist">
+        <div class="active"></div>
+            <table border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>Last Name</th>
+                    <th>First Name</th>
+                    <th>Email</th>
+                    <th>Action</th>
+                </tr>
+    
+    
+
+            <?php
+            include 'db.php';
+
+            $result = $conn->query("SELECT * FROM users"); 
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>
+                        <td>{$row['id']}</td>
+                        <td>{$row['lname']}</td>
+                        <td>{$row['fname']}</td>
+                        <td>{$row['email']}</td>
+                        <td>
+                            <div class= ActionCase>
+
+                               <div class=Editbtn>
+                                    <a href='edit.php?id={$row['id']}' class='Link'>Edit</a>
+                                </div>
+
+                                <div class=Delbtn>
+                                    <a href='delete.php?id={$row['id']}' class='Link'>Delete</a>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>";
+            }
+            ?>
+
+        </table>
+        
+        </div>
+</div>
+    
+   
  
 </body>
 </html>
