3ea7e9b18e3e7c521d8e4b39adb14f75237f839e
Markk-dev <markvincent.dev@gmail.com>
2024-10-16 03:41:59 +0800

    Added css for users

--- Main/users.php
+++ Main/users.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users</title>
-    <link rel="stylesheet" href="./Sub/user.css">
+    <link rel="stylesheet" href="../Sub/user.css">
 </head>
 <body>
     <h2>Registered Users</h2>
